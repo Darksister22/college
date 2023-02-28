6fe6925555862c8180306d033d8ab25bc8b3ecc3
Darksister <fatema@ursoft.com>
2023-02-28 13:10:42 +0300

    simple look and feel changes

--- lib/screens/studenttable.dart
+++ lib/screens/studenttable.dart
@@ -19,79 +19,16 @@ class _StudentTableState extends State<StudentTable> {
     double width = MediaQuery.of(context).size.width;
     double height = MediaQuery.of(context).size.height;
     return Dashboard(
-      selectedRoute: '/studenttable',
+      selectedRoute: '/studentmenu',
       title: "معلومات الطلبة",
       child: SingleChildScrollView(
         child: Padding(
           padding: const EdgeInsets.all(20.0),
-          // child: Column(
-          //   crossAxisAlignment: CrossAxisAlignment.start,
-          //   children: [
-          //     sizedBox(height: 30.0),
-          //     Row(
-          //       children: [
-          //         mainSurface(context, "الرجاء اختيار المرحلة لعرض الطلبة"),
-          //         const Spacer(),
-          //         iconLabelButton(() {
-          //           showDialog(
-          //               context: context, builder: (context) => AddStudent());
-          //         }, "طالب جديد", FontAwesomeIcons.plus),
-          //         sizedBox(width: 40.0)
-          //       ],
-          //     ),
-          //     sizedBox(height: 40.0),
-          //     Row(
-          //       mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          //       children: [
-          //         buttonCard(
-          //             context,
-          //             width / 5.5,
-          //             height / 7,
-          //             const FaIcon(FontAwesomeIcons.one),
-          //             "المرحلة الاولى",
-          //             () {}),
-          //         buttonCard(
-          //             context,
-          //             width / 5.5,
-          //             height / 7,
-          //             const FaIcon(FontAwesomeIcons.two),
-          //             "المرحلة الثانية",
-          //             () {}),
-          //         buttonCard(
-          //             context,
-          //             width / 5.5,
-          //             height / 7,
-          //             const FaIcon(FontAwesomeIcons.three),
-          //             "المرحلة الثالثة",
-          //             () {}),
-          //       ],
-          //     ),
-          //     sizedBox(height: 30.0),
-          //     Row(
-          //       mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          //       children: [
-          //         buttonCard(
-          //             context,
-          //             width / 5.5,
-          //             height / 7,
-          //             const FaIcon(FontAwesomeIcons.four),
-          //             "المرحلة الرابعة",
-          //             () {}),
-          //         buttonCard(
-          //             context,
-          //             width / 5.5,
-          //             height / 7,
-          //             const FaIcon(FontAwesomeIcons.five),
-          //             "المرحلة الخامسة",
-          //             () {}),
-          //       ],
-          //     ),
-          //   ],
-          // ),
           child: DynamicTable(
               source: DynamicDataTableSource(
                   cells: (currentRowData) {
                     return [
+                      const DataCell(FaIcon(FontAwesomeIcons.pen)),
                       dataCell(currentRowData["name_ar"].toString()),
                       dataCell(currentRowData["name_en"].toString()),
                       dataCell(currentRowData["year"].toString()),
@@ -99,16 +36,10 @@ class _StudentTableState extends State<StudentTable> {
                   },
                   uri: "/api/students"),
               columns: [
+                dataColumn("تعديل", context),
                 dataColumn("اسم الطالب", context),
                 dataColumn("Student's Name", context),
                 dataColumn("Year", context),
-                // dataColumn(AppLocalizations.of(context)!.phone, context,
-                //     numeric: true),
-                // dataColumn(AppLocalizations.of(context)!.branch, context),
-                // dataColumn(AppLocalizations.of(context)!.credit, context,
-                //     numeric: true),
-                // dataColumn(AppLocalizations.of(context)!.debt, context,
-                //     numeric: true),
               ]),
         ),
       ),
